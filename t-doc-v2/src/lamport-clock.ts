f4b6f48c15547ccc8273f7ced0d039358fec081e
cgoing <neo.cgoing@gmail.com>
2024-08-15 23:18:35 +0900

    added logical clock

---
+++ t-doc-v2/src/lamport-clock.ts
@@ -0,0 +1,12 @@
+export class LamportClock {
+  constructor(private time = 0) {}
+  tick(): number {
+    return ++this.time;
+  }
+  update(time: number): void {
+    this.time = Math.max(time, this.time) + 1;
+  }
+  getTime(): number {
+    return this.time;
+  }
+}
